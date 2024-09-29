7ad6325ec6ec457a84a498d3266f6675d635b2ce
Jairo2santos <107949575+Jairo2santos@users.noreply.github.com>
2024-09-29 10:36:10 -0300

    Update page.tsx

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -12,12 +12,11 @@ async function Page() {
   if (userInfo?.onboarded) redirect("/");
 
   const userData = {
-    id: user.id,
-    objectId: userInfo?._id,
-    username: userInfo ? userInfo?.username : user.username,
-    name: userInfo ? userInfo?.name : user.firstName ?? "",
-    bio: userInfo ? userInfo?.bio : "",
-    image: userInfo ? userInfo?.image : user.imageUrl,
+    id: user?.id,
+    objectId: userInfo?._id ? userInfo._id : null,  // Verificación más segura
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
   };
 
   return (
